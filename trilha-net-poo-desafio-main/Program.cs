a035813784d42de1980dc9b739f9e2203b404d36
Deyvid Ataíde <118136715+devdags@users.noreply.github.com>
2024-01-14 20:13:36 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DesafioPOO.Models; 
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Nokia nokia = new Nokia("123456789", "Nokia Model X", "IMEI123", 64);
+        nokia.InstalarAplicativo("Facebook");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        Console.WriteLine("\nSmartphone Iphone:");
+        Iphone iphone = new Iphone("987654321", "iPhone 12", "IMEI456", 128);
+        iphone.InstalarAplicativo("Teams");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+    }
+}
